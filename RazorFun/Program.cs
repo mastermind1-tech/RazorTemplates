1ccd19eff26915578f95ddb67aa81c7ef103b599
Damian Edwards <damian@damianedwards.com>
2025-11-25 07:47:39 -0800

    Refactor some things

--- RazorFun/Program.cs
+++ RazorFun/Program.cs
@@ -12,13 +12,13 @@ app.MapGet("/", () => Results.Razor(Templates.Home()));
 app.MapGet("/string", async (IServiceProvider services, string name = "World") =>
 {
     var user = new User { Name = name, Age = 32 };
-    return await Templates.RenderCard(user).RenderAsync(services);
+    return await Templates.Card(user).RenderAsync(services);
 });
 
 app.MapGet("/iresult", (string name = "World") =>
 {
     var user = new User { Name = name, Age = 32 };
-    return Results.Razor(Templates.RenderCard(user));
+    return Results.Razor(Templates.Card(user));
 });
 
 app.Run();
--- RazorTemplates/RenderFragmentExtensions.cs
+++ RazorTemplates/RenderFragmentExtensions.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Components.Web;
+﻿using Microsoft.AspNetCore.Components.RenderTree;
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using RazorTemplates;
@@ -9,15 +10,26 @@ public static class RenderFragmentExtensions
 {
     extension(RenderFragment fragment)
     {
+        public async Task<string> RenderAsync(IServiceProvider services, HtmlRenderer renderer)
+        {
+            var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+            return await RenderImpl(fragment, renderer);
+        }
+
         public async Task<string> RenderAsync(IServiceProvider services)
         {
             var loggerFactory = services.GetRequiredService<ILoggerFactory>();
             using var renderer = new HtmlRenderer(services, loggerFactory);
-            var parameters = new FragmentComponent.ParametersDictionary(fragment);
-            var parameterView = ParameterView.FromDictionary(parameters);
-            var root = await renderer.Dispatcher.InvokeAsync(() => renderer.RenderComponentAsync<FragmentComponent>(parameterView));
-            await root.QuiescenceTask;
-            return await renderer.Dispatcher.InvokeAsync(root.ToHtmlString);
+            return await RenderImpl(fragment, renderer);
         }
+    }
+
+    private static async Task<string> RenderImpl(RenderFragment fragment, HtmlRenderer renderer)
+    {
+        var parameters = new FragmentComponent.ParametersDictionary(fragment);
+        var parameterView = ParameterView.FromDictionary(parameters);
+        var root = await renderer.Dispatcher.InvokeAsync(() => renderer.RenderComponentAsync<FragmentComponent>(parameterView));
+        await root.QuiescenceTask;
+        return await renderer.Dispatcher.InvokeAsync(root.ToHtmlString);
     }
 }
